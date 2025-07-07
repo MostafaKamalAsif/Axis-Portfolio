0865b26d6c1e61025974cf214cf1b686151ec5ed
MostafaKamalAsif <mostafakamal483905@gmail.com>
2025-07-07 11:48:16 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//  counter up start  
+     
+        jQuery(document).ready(function($) {
+            $('.counter').counterUp({
+                delay: 20,
+                time: 1500
+            });
+        });
+   
+    //  counter up End  
